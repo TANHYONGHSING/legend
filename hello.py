8c5996f6cc9271ae31ed5052fe93bdfc46e0bce5
Tan Hyong Hsing <tanhyonghsing2gmail.com>
2023-02-07 16:07:17 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,38 @@
+#Tan Hyong Hsing
+#010-2983933
+#tanhyonghsing@gmail.com
+#name = input("what is your name?\n")
+#greeting = "Hello" + " " + name
+#print(greeting)
+#height = float(input("What is your height ?\n"))
+#weight = float(input("What is your weight ?\n"))
+#bmi = weight / (height*height)
+#bmi = weight / (height*height)
+#print(name + " " +",your BMI is" + str(bmi))
+
+# Develop a software to calculate to calculate Sales
+# 1) Ask name of a user
+# 2) Ask for How many percent is the Sales tax 
+# sales tax = percentage of tax * price amount of good
+# 3) The output: "HI xxxx , the total amount to pay is xxx, the sales tax is xxx"
+# Sales amount = total sales + sales tax
+# barang $1500 tax 6.25%  maka nilai keseluruhan ialah 1631.25"
+
+#Tan Hyong Hsing
+# Develop a software to calculate to calculate Sales
+#user = input("what is user name ?\n")
+#tax = float(input("How many percent is sales tax ?\n"))
+#price = float(input("How much is the price amount of good ?\n"))
+#tax1 = (tax/100) * price 
+#salesamount = price + tax1
+#print("HI" + " " + user + ", the total amount to pay is "+str(price)+",the sales tax is"+str(tax1))
+#print("The total sales amount is" + str(salesamount))
+
+#Tan Hyong Hsing
+#20DDT21F1002
+
+name = input("What is your name ? \n")
+celsius = float (input("Hi"+" " + name +",please give the current temperature in celsius.\n"))
+#fahrenheit = celsius * 9/5 + 32
+fahrenheit = (celsius * 1.8) + 32
+print("The current temperature in fahrenheit is " +str(fahrenheit))
