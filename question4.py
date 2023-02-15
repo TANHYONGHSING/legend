1b36b8ee73dffa1dee1c63977f7b4b461709c65f
Tan Hyong Hsing <tanhyonghsing@gmail.com>
2023-02-15 19:29:27 +0800

    FA

---
+++ question4.py
@@ -0,0 +1,10 @@
+#Tan Hyong Hsing
+#20DDT21F1002
+
+centimeters = float(input("Input the length in centimeters\n"))
+if centimeters >= 0 :
+    inches = centimeters / 2.54
+    print(inches , "inch")
+else :
+    print("Sorry , your input is invalid . Please try again . ")
+print("Thank you for using our system.")
