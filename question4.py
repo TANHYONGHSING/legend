9c5052d495696eceb4cc01da2ac0685c4fed1d6b
Tan Hyong Hsing <tanhyonghsing@gmail.com>
2023-02-20 15:51:40 +0800

    fa

--- question4.py
+++ question4.py
@@ -4,7 +4,14 @@
 centimeters = float(input("Input the length in centimeters\n"))
 if centimeters >= 0 :
     inches = centimeters / 2.54
-    print(inches , "inch")
+    two_decimal = round(inches ,2)
+    print(two_decimal , "inch")
 else :
     print("Sorry , your input is invalid . Please try again . ")
+#if centimeters < 0 :
+    #  print("Sorry , your input is invalid . Please try again . ")
+#else :
+    #   inches = centimeters / 2.54
+    #   print(inches , "inch")
+  
 print("Thank you for using our system.")
--- question5.py
+++ question5.py
@@ -1,17 +1,20 @@
 # Tan Hyong Hsing
 # 20DDT21F1002
 
-#password = '20DDT21F1002'
-password = input('Please input your password. \n')
-count = 0
-if password == '20DDT21F1002' :
-    print('Login Successful !')
-else :
-    while (count < 4):
-        count = count + 1
-        password = input('Please input your password. \n')
-    else : 
-        print('You are blocked of the system after five unsuccessful try . Please Try Again .')
+password = '20DDT21F1002'
+max_attempt = 5
+i = 0
 
+while i < max_attempt:
+    guess = input('Please input your password.')
+    i = i + 1
+    if guess == password:
+        print('Login Successful !')
+        break
+    #break utk terminate terus system klau pass betul
+    else :
+        print('Wrong password, Please re-enter.')
+else :
+    print('You are blocked from the system .')
 
 
