b23980176286de8c55742f09df545e0994e973dc
Tan Hyong Hsing <tanhyonghsing@gmail.com>
2023-02-27 22:59:25 +0800

    mod

--- extra_exercise5.py
+++ extra_exercise5.py
@@ -2,46 +2,141 @@
 #20DDT21F1002
 #POS System in a supermarket.
 
-#Request for how many items the customer bought
-print('Only limit for 3 items')
-total_items = int(input('How many items the customer bought ? \n'))
+name = input('Welcome ! What is your name ?\n')
+print('Hi ' + name + ' , each customer only allowed to purchase 5 items !')
+total_items = int(input('How many items the customer purchase ? \n'))
 
 i = 0
 while i < total_items:
-    #price = float(input('price ? \n'))
+
     i = i + 1
     if total_items == 1:
-        price1 = float(input('price ?'))
-        grand_total = price1
-        amount = float(input('how much user paid ?'))
+        price1 = float(input('How much does the item 1 cost ? \n'))
+        grand_total = price1 
+        amount = float(input('The amount of money given by the customer .\n'))
         balance = amount - grand_total
-        print('the grand_total is RM ' + str(grand_total))
-        print('the amount user paid is RM ' + str(amount))
-        print('the balance for user is RM ' + str(balance))
+        print("                   Tan's Supermarket                         ")
+        print('                         MUKAH                               ')
+        print('                        RECEIPT                              ')
+        print('-------------------------------------------------------------')
+        print('Cashier : ' + name +'\n')
+        print(' Description                                           Price      ')
+        print('-------------------------------------------------------------')
+        print('1  Item 1                                               '+str(price1))
+        print('-------------------------------------------------------------')
+        print('                                        GRAND TOTAL :RM '+str(grand_total))
+        print('-------------------------------------------------------------')
+        print('                                      Cash Received :RM '+str(amount))
+        print('                                            Balance :RM '+str(balance)+'\n')
+        print('                    ***Thank You***                          ')
+        print('                   Please Come Again                         ')
         break
     elif total_items == 2:
-        price1 = float(input('price ?'))
-        price2 = float(input('price ?'))
+        price1 = float(input('How much does the item 1 cost ? \n'))
+        price2 = float(input('How much does the item 2 cost ? \n'))
         grand_total = price1 + price2
-        amount = float(input('how much ?'))
+        amount = float(input('The amount of money given by the customer .\n'))
         balance = amount - grand_total
-        print('the grand_total is RM ' + str(grand_total))
-        print('the amount user paid is RM ' + str(amount))
-        print('the balance for user is RM ' + str(balance))
+        print("                   Tan's Supermarket                         ")
+        print('                         MUKAH                               ')
+        print('                        RECEIPT                              ')
+        print('-------------------------------------------------------------')
+        print('Cashier : ' + name +'\n')
+        print(' Description                                           Price      ')
+        print('-------------------------------------------------------------')
+        print('1  Item 1                                               '+str(price1))
+        print('2  Item 2                                               '+str(price2))
+        print('-------------------------------------------------------------')
+        print('                                        GRAND TOTAL :RM '+str(grand_total))
+        print('-------------------------------------------------------------')
+        print('                                      Cash Received :RM '+str(amount))
+        print('                                            Balance :RM '+str(balance)+'\n')
+        print('                    ***Thank You***                          ')
+        print('                   Please Come Again                         ')
         break
     elif total_items == 3:
-        price1 = float(input('price ?'))
-        price2 = float(input('price ?'))
-        price3 = float(input('price ?'))
+        price1 = float(input('How much does the item 1 cost ? \n'))
+        price2 = float(input('How much does the item 2 cost ? \n'))
+        price3 = float(input('How much does the item 3 cost ? \n'))
         grand_total = price1 + price2 + price3
-        amount = float(input('how much ?'))
+        amount = float(input('The amount of money given by the customer .\n'))
         balance = amount - grand_total
-        print('the grand_total is RM ' + str(grand_total))
-        print('the amount user paid is RM ' + str(amount))
-        print('the balance for user is RM ' + str(balance))
+        print("                   Tan's Supermarket                         ")
+        print('                         MUKAH                               ')
+        print('                        RECEIPT                              ')
+        print('-------------------------------------------------------------')
+        print('Cashier : ' + name +'\n')
+        print(' Description                                           Price      ')
+        print('-------------------------------------------------------------')
+        print('1  Item 1                                               '+str(price1))
+        print('2  Item 2                                               '+str(price2))
+        print('3  Item 3                                               '+str(price3))
+        print('-------------------------------------------------------------')
+        print('                                        GRAND TOTAL :RM '+str(grand_total))
+        print('-------------------------------------------------------------')
+        print('                                      Cash Received :RM '+str(amount))
+        print('                                            Balance :RM '+str(balance)+'\n')
+        print('                    ***Thank You***                          ')
+        print('                   Please Come Again                         ')
+        break
+    elif total_items == 4:
+        price1 = float(input('How much does the item 1 cost ? \n'))
+        price2 = float(input('How much does the item 2 cost ? \n'))
+        price3 = float(input('How much does the item 3 cost ? \n'))
+        price4 = float(input('How much does the item 4 cost ? \n'))
+        grand_total = price1 + price2 + price3 + price4
+        amount = float(input('The amount of money given by the customer . \n'))
+        balance = amount - grand_total
+        print("                   Tan's Supermarket                         ")
+        print('                         MUKAH                               ')
+        print('                        RECEIPT                              ')
+        print('-------------------------------------------------------------')
+        print('Cashier : ' + name +'\n')
+        print(' Description                                           Price      ')
+        print('-------------------------------------------------------------')
+        print('1  Item 1                                               '+str(price1))
+        print('2  Item 2                                               '+str(price2))
+        print('3  Item 3                                               '+str(price3))
+        print('4  Item 4                                               '+str(price4))
+        print('-------------------------------------------------------------')
+        print('                                        GRAND TOTAL :RM '+str(grand_total))
+        print('-------------------------------------------------------------')
+        print('                                      Cash Received :RM '+str(amount))
+        print('                                            Balance :RM '+str(balance)+'\n')
+        print('                    ***Thank You***                          ')
+        print('                   Please Come Again                         ')
+        break
+    elif total_items == 5:
+        price1 = float(input('How much does the item 1 cost ? \n'))
+        price2 = float(input('How much does the item 2 cost ? \n'))
+        price3 = float(input('How much does the item 3 cost ? \n'))
+        price4 = float(input('How much does the item 4 cost ? \n'))
+        price5 = float(input('How much does the item 5 cost ? \n'))
+        grand_total = price1 + price2 + price3 + price4 + price5
+        amount = float(input('The amount of money given by the customer . \n'))
+        balance = amount - grand_total
+        print("                   Tan's Supermarket                         ")
+        print('                         MUKAH                               ')
+        print('                        RECEIPT                              ')
+        print('-------------------------------------------------------------')
+        print('Cashier : ' + name +'\n')
+        print(' Description                                           Price      ')
+        print('-------------------------------------------------------------')
+        print('1  Item 1                                               '+str(price1))
+        print('2  Item 2                                               '+str(price2))
+        print('3  Item 3                                               '+str(price3))
+        print('4  Item 4                                               '+str(price4))
+        print('5  Item 5                                               '+str(price5))
+        print('-------------------------------------------------------------')
+        print('                                        GRAND TOTAL :RM '+str(grand_total))
+        print('-------------------------------------------------------------')
+        print('                                      Cash Received :RM '+str(amount))
+        print('                                            Balance :RM '+str(balance)+'\n')
+        print('                    ***Thank You***                          ')
+        print('                   Please Come Again                         ')
         break
     else :
-        print('exceeded amount of purchase')
+        print('You exceeded the limit amount of purchase item .')
         break
 
 
