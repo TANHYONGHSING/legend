ecef723cc6cdc93ad5668f509f8420ab7f1c4332
Tan Hyong Hsing <tanhyonghsing@gmail.com>
2023-02-27 13:19:23 +0800

    3

---
+++ extra_exercise3.py
@@ -0,0 +1,8 @@
+#Tan Hyong Hsing
+#20DDT21F1002
+#To count how many characters it has based on the input entered by user
+
+characters = input ('Please enter a characters . \n')
+print(characters + ' have ' + str(len(characters)) + ' characters . ')
+
+
